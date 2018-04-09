4e2c4d741bb1b2a513991675b3c99ea29fa86df1
bigrocs <bigrocs@qq.com>
2018-04-09 15:24:10 +0800

    增加一些依赖包比如：axios 等

--- src/main.js
+++ src/main.js
@@ -1,9 +1,2 @@
 // 引入依赖
-import builderVue from 'builder-vue'
-// 引用
-import BuilderVueElement from 'builder-vue-element'
-import ContainerVueElement from 'container-vue-element'
-builderVue.Vue.use(BuilderVueElement)
-builderVue.Vue.use(ContainerVueElement)
-// 启动自动构建
-builderVue.start()
+import builderVue from '../package/Json-vue/src/index.js'
