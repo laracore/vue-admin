71b818f8355a10c8364e250be9b07a9882c121de
bigrocs <bigrocs@qq.com>
2018-04-20 15:44:42 +0800

    增加 ContainerVueIview 模块

--- src/main.js
+++ src/main.js
@@ -1,3 +1,8 @@
 // 引入依赖
+import Vue from 'vue'
 import JsonVue from '../packages/json-vue/src/index.js'
+import ContainerVueIview from '../packages/container-vue-iview/src/index.js'
+
+Vue.use(ContainerVueIview)
+
 JsonVue.start()
