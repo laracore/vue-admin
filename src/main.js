26a3303b91a708811d238be4f71a22c4e57f6343
bigrocs <bigrocs@qq.com>
2018-05-16 15:05:39 +0800

    增加 JsonVueIview

--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@
 import Vue from 'vue'
 import JsonVue from '../packages/json-vue/src/index.js'
 import ContainerVueIview from '../packages/container-vue-iview/src/index.js'
+import JsonVueIview from '../packages/json-vue-iview/src/index.js'
 
 Vue.use(ContainerVueIview)
+Vue.use(JsonVueIview)
+
 JsonVue.start()
